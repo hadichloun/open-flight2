9454841b0c004181fd3fa0a6230e15a39a54112d
hadi <hadi_960@hotmail.com>
2021-09-08 16:22:16 -0500

    adds relationship to airline and adds slug

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,2 +1,12 @@
 class Airline < ApplicationRecord
+    has_many :reviews
+
+    before_create :slugify
+
+    def slugify
+        self.slug = name.parameterize
+    end
+
+    def avg_score
+        reviews.
 end
